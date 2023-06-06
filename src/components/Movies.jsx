1af778afcc4eed731b7f472d81441ae404dfef82
Mukesh Kumar <muk.786422@gmail.com>
2023-06-06 10:46:12 +0530

    Movies and Banner Component Completed

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,64 +1,96 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
+import axios from 'axios';
 import maya from '../assets/maya.webp';
+import { Oval } from 'react-loader-spinner';
+
+
 
 
 const Movies = () => {
+
+
+
+
+  let [movies , setMovies] = useState([]);
+
+
+  useEffect(function(){
+
+
+    (function () {
+     axios.
+     get('https://api.themoviedb.org/3/trending/all/week?language=en-US&api_key=c329e49ea66541c60669781b65c43433&page=1')
+     .then((res)=>{
+      console.log(res.data.results);
+      setMovies(res.data.results)
+
+
+     })
+   }) () 
+ } ,[])
+   
+
+
+
+
+
+
+
   return (
+
     <div className='mt-8'>  
      <div className=' mb-8 font-bold text-2xl text-center  '>Trendings</div>
+     <div className='flex justify-center  flex-wrap '> 
+
+
+ {
+
+  movies.length == 0 ? <div className='flex justify-center'>  
+  <Oval
+ height="80"
+ width="80"
+ radius="9"
+ color="gray"
+ secondaryColor='gray'
+ ariaLabel="loading"
+ wrapperStyle
+ wrapperClass
+/>  </div> 
+   : 
+
+  movies.map((movie)=> { 
+  
+return ( 
+    <div  key = {movie.id} className='flex justify-center  flex-wrap '> 
+    <div className='w-[160px] h-[30vh] md:h-[40vh] md:w-[180px]  rounded-xl m-4 hover:scale-110 duration-300 border-4   
+    bg-center
+    bg-cover 
+    flex items-end  relative' 
+    
+    style={{
+      backgroundImage:`url(https://image.tmdb.org/t/p/original/t/p/original/${movie.poster_path})`
+    }}  >
+
+    <div className=' text-xl text-white  bg-gray-900 p-2 bg-opacity-100  text-center w-full  rounded-b-xl  font-bold'>
+    {movie.title || movie.name}
+    </div>
+
+    </div>
+
+    
+      
+
+</div>
+ )
+
+  })
+
+
+ }  </div>
+
+
+
      
-      <div className='flex justify-center  flex-wrap '>
-          <div className='flex justify-center  flex-wrap'> 
-          <div className='w-[160px] h-[30vh] md:h-[40vh] md:w-[180px]  rounded-xl m-4 hover:scale-110 duration-300 border-4   
-          bg-center
-          bg-cover 
-          bg-[url(https://www.unesco.org/sites/default/files/styles/paragraph_medium_desktop/public/2022-04/godwin-angeline-benjo-An7LvVMb6rY-unsplash.jpeg?itok=3STf8JD9)]   flex items-end'   >
-
-          <div className=' text-xl text-white  bg-gray-900 p-2 bg-opacity-100  text-center w-full  rounded-b-xl  font-bold'>
-          M3GAN
-          </div>
-
-          </div>
-
-          <div className='w-[160px] h-[30vh] md:h-[40vh] md:w-[180px]  rounded-xl m-4 hover:scale-110 duration-300 border-4   
-          bg-center
-          bg-cover 
-          bg-[url(https://www.unesco.org/sites/default/files/styles/paragraph_medium_desktop/public/2022-04/godwin-angeline-benjo-An7LvVMb6rY-unsplash.jpeg?itok=3STf8JD9)]   flex items-end'   >
-
-          <div className=' text-xl text-white  bg-gray-900 p-2 bg-opacity-100  text-center w-full  rounded-b-xl  font-bold'>
-          M3GAN
-          </div>
-
-          </div>
-
-          <div className='w-[160px] h-[30vh] md:h-[40vh] md:w-[180px]  rounded-xl m-4 hover:scale-110 duration-300 border-4   
-          bg-center
-          bg-cover 
-          bg-[url(https://www.unesco.org/sites/default/files/styles/paragraph_medium_desktop/public/2022-04/godwin-angeline-benjo-An7LvVMb6rY-unsplash.jpeg?itok=3STf8JD9)]   flex items-end'   >
-
-          <div className=' text-xl text-white  bg-gray-900 p-2 bg-opacity-100  text-center w-full  rounded-b-xl  font-bold'>
-          M3GAN
-          </div>
-
-          </div>
-
-          <div className='w-[160px] h-[30vh] md:h-[40vh] md:w-[180px]  rounded-xl m-4 hover:scale-110 duration-300 border-4   
-          bg-center
-          bg-cover 
-          bg-[url(https://www.unesco.org/sites/default/files/styles/paragraph_medium_desktop/public/2022-04/godwin-angeline-benjo-An7LvVMb6rY-unsplash.jpeg?itok=3STf8JD9)]   flex items-end'   >
-
-          <div className=' text-xl text-white  bg-gray-900 p-2 bg-opacity-100  text-center w-full  rounded-b-xl  font-bold'>
-          M3GAN
-          </div>
-
-          </div>
-            
-
-            </div>
-
-          
-      </div>
-
 
       
     
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     
     <div  className = "border flex items-center space-x-2  pl-12 py-2">
-    <img src={Logo} className='w-[70px] h-[50px]'/>
+    <img src={Logo} className='w-[70px] h-[50px]' alt='q'/>
     <h3 className='font-bold  text-xl   text-blue-400'> Movies </h3>
     <h3 className='font-bold   text-xl   text-blue-400'> Favorites</h3>
     </div>
