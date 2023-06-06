598471bf51e8880e0291143438c533cb676473f9
Mukesh Kumar <muk.786422@gmail.com>
2023-06-06 18:23:30 +0530

    Emojis added for Favourites Movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -11,21 +11,13 @@ const Movies = () => {
 
  let [movies , setMovies] = useState([]);
   const [pageNum  , setPage]  =  useState(1);
-
-  const onPrev =()=>{
-
-   if(pageNum > 1){
-         setPage( pageNum - 1 ); 
-        }
-    }
+  const [hovered ,  setHovered ] = useState();
+  let [ favourites ,  setFavourites] = useState([]);
+  
 
 
-    const onNext =()=>{
-            setPage( pageNum + 1 );            }
-       
-   
 
-
+  /* This for Only Making Api request   */
   useEffect(function(){
 
 
@@ -43,6 +35,53 @@ const Movies = () => {
    
 
 
+ /* Pagination Handeling */
+  const onPrev =()=>{
+
+   if(pageNum > 1){
+         setPage( pageNum - 1 ); 
+        }
+    }
+
+
+    const onNext =()=>{
+            setPage( pageNum + 1 );            }
+       
+   
+
+            /* emoji Handelin */
+
+    const showEmoji = (id) =>{
+      setHovered(id) ; 
+    }
+
+
+    const hideEmoji = () =>{
+      setHovered("") ; 
+    }
+
+
+    /* Add emoji and Remove emoji */
+
+    const addEmoji =(id)=>{
+     const newfav =  [...favourites , id] 
+     setFavourites(newfav);
+      
+
+
+    }
+
+
+ const removeEmoji =(id)=>{
+
+
+  const filteredFav = favourites.filter(elem => {
+    return elem != id ;
+  })
+
+  setFavourites(filteredFav);
+  
+    }
 
 
 
@@ -71,7 +110,17 @@ const Movies = () => {
   movies.map((movie)=> { 
   
 return ( 
-    <div  key = {movie.id} className='flex justify-center  flex-wrap '> 
+    <div   onMouseOver={()=>{
+      showEmoji(movie.id)
+    }}   
+
+
+
+    onMouseLeave={()=>{
+      hideEmoji(movie.id)
+    }}   
+    
+    key = {movie.id} className='flex justify-center  flex-wrap '> 
     <div className='w-[160px] h-[30vh] md:h-[40vh] md:w-[180px]  rounded-xl m-4 hover:scale-110 duration-300 border-4   
     bg-center
     bg-cover 
@@ -80,6 +129,40 @@ return (
     style={{
       backgroundImage:`url(https://image.tmdb.org/t/p/original/t/p/original/${movie.poster_path})`
     }}  >
+
+
+    <div className='bg-gray-900  p-2  absolute top-2 right-2 rounded-xl'
+    
+    style={{
+
+      display : hovered == movie.id ? "block" : "none" 
+    }}
+    >
+
+    {
+    favourites.includes(movie.id) == false ? <div 
+    className='text-2xl' 
+
+    onClick={()=>{
+      addEmoji(movie.id)}}
+      
+      >🥰</div> : <div
+
+    onClick={()=>{
+      removeEmoji(movie.id)}}
+
+      className='text-2xl'>❌</div>
+
+    }
+    
+     
+    
+    
+    </div>
+
+
+
+
 
     <div className=' text-xl text-white  bg-gray-900 p-2 bg-opacity-100  text-center w-full  rounded-b-xl  font-bold'>
     {movie.title || movie.name}
