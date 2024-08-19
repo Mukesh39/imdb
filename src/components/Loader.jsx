79beccef4c7b7b25fd3538b75bcab00e7d0371e1
Mukesh Kumar <mukeshkumar@192.168.1.4>
2024-08-19 22:07:01 +0530

    changes done is  Loaders style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="Loader-spinner">
-      <button type="button" class="bg-indigo-500 ..." disabled>
+      <button type="button"  className="bg-indigo-500 ... p-3 mt-2 mb-2 ml-2 mr-2 text-white  text-bold rounded-xl w-80 " disabled>
         <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
         Processing...
       </button>
